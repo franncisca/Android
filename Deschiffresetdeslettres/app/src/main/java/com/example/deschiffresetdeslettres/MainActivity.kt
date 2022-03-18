944bc3d73dd1d0f09a20addda00e83fe7226ee7d
LI YAN <yan.li@localhost.localdomain>
2022-03-18 19:30:50 +0100

    feat: display answer

--- Deschiffresetdeslettres/app/src/main/java/com/example/deschiffresetdeslettres/MainActivity.kt
+++ Deschiffresetdeslettres/app/src/main/java/com/example/deschiffresetdeslettres/MainActivity.kt
@@ -5,15 +5,18 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
-    val letterPicker by lazy { LetterPicker.buildFromResource(this, R.raw.letter_frequencies)}
+    val letterPicker by lazy { LetterPicker.buildFromResource(this, R.raw.letter_frequencies) }
     val drawTextView by lazy { findViewById<TextView>(R.id.drawTextView) }
     val startButton by lazy { findViewById<Button>(R.id.startButton) }
     val validButton by lazy { findViewById<Button>(R.id.validButton) }
     val propositionText by lazy { findViewById<EditText>(R.id.proposition) }
+    val answerText by lazy { findViewById<TextView>(R.id.answer) }
 
     var bestAnagrams = listOf<String>();
+    var biggestSize = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,35 +25,33 @@ class MainActivity : AppCompatActivity() {
         //drawTextView.setOnClickListener{drawTextView.text = letterPicker.pickLetters(9)}
         //Log.d( "msg: ",s)}
 
-        startButton.setOnClickListener{drawTextView.text = letterPicker.pickLetters(9)}
+        startButton.setOnClickListener { drawTextView.text = letterPicker.pickLetters(9) }
 
-        validButton.setOnClickListener{
-            p -> {
-                var last = propositionText.text
-
-                if (propositionText.text.length > last.length){
-                    last = propositionText.text
-                }
+        validButton.setOnClickListener {
+            var currentText = propositionText.text
+            if (currentText.length >= biggestSize) {
+                biggestSize = currentText.length;
+                bestAnagrams = bestAnagrams + listOf<String>(currentText.toString())
+                answerText.text = bestAnagrams.toString();
             }
         }
-
-
-    }
-
-    /* Determine if the parameter is an anagram of `this`
- * proposition must contain only letters of `this` (with the same or different order)
- */
-    fun String.containsAnagram(proposition: String): Boolean
-    {
-        var answer = "answer";
-        for (s in proposition){
-            if (s !in answer){
-                return false
-            }
-
-        }
-        return false
     }
 
 
 }
+
+/* Determine if the parameter is an anagram of `this`
+* proposition must contain only letters of `this` (with the same or different order)
+*/
+fun String.containsAnagram(proposition: String): Boolean {
+    var answer = "answer";
+    for (s in proposition) {
+        if (s !in answer) {
+            return false
+        }
+
+    }
+    return false
+}
+
+
